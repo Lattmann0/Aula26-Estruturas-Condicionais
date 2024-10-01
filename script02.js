288a9067c5cfc987948fe01cfb34e1f93ea43eeb
Guilherme Lattmann <guilhermelattmann@hotmail.com>
2024-09-30 21:58:02 -0300

    First commit

--- script02.js
+++ script02.js
@@ -1,1 +1,19 @@
-//
+const comando  = prompt("Informe o comando para a lampada (ligar/desligar")
+
+const elementoImagem = document.createElement("img")
+
+
+switch (comando.toLocaleLowerCase) {
+    case "ligar":
+        elementoImagem.src = "./"
+        break;
+    case "desligar":
+        
+        break;
+    default:
+        break;
+}
+
+if (elementoImagem.src != null) {
+    document.body.appendChild(elementoImagem)
+}
